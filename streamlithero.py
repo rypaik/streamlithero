f4490ef4f91ba7bec8a746043e80814c0cbf1a8d
Ryan paik <ryanpaik@protonmail.com>
2022-02-23 21:00:23 -0800

    first commit

---
+++ streamlithero.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('Hello World. Another Title Page')
+
+# Subheader
+st.subheader('Here, a subheader')
+
+# Text
+st.text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum.')
+# Using st.write
+
+st.write('This is can be used for text and other features.')
